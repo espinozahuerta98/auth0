ac13d42202461bf61f457f11fce245f00d8f8c68
Carlos Daniel Espinoza Huerta <espinozahuerta98@gmail.com>
2021-03-07 12:47:08 -0500

    auth0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {AuthModule} from '@auth0/auth0-angular'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PreciosComponent } from './components/precios/precios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProtegidaComponent,
+    PreciosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'carlos-espinoza.us.auth0.com',
+      clientId: '485IdFpqIm3LZSTeP5LZEIqnJfXnmvfx'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
